21406150d060d75be8fc58ae7f17474dcce50285
Leo Zeng <qingchun1995@gmail.com>
2021-08-08 21:18:13 +0800

    ci: add ESLint, Prettier, Tailwind CSS and Commitlint

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,25 @@
+const CracoLessPlugin = require('craco-less');
+
 module.exports = {
   style: {
     postcss: {
       plugins: [require('tailwindcss'), require('autoprefixer')]
     }
-  }
+  },
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            javascriptEnabled: true,
+            modifyVars: {
+              '@primary-color': 'rgb(0, 82, 204)',
+              '@font-size-base': '14px'
+            }
+          }
+        }
+      }
+    }
+  ]
 };
