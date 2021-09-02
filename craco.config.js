3565eae902c68ca611b0fd9291fd78b05853ed98
Leo Zeng <qingchun1995@gmail.com>
2021-09-02 14:19:42 +0800

    :wrench:  config: update the CRACO config file

--- craco.config.js
+++ craco.config.js
@@ -1,25 +1,23 @@
-const CracoLessPlugin = require('craco-less');
+import CracoLessPlugin from 'craco-less';
 
-module.exports = {
-  style: {
-    postcss: {
-      plugins: [require('tailwindcss'), require('autoprefixer')]
-    }
-  },
-  plugins: [
-    {
-      plugin: CracoLessPlugin,
-      options: {
-        lessLoaderOptions: {
-          lessOptions: {
-            javascriptEnabled: true,
-            modifyVars: {
-              '@primary-color': 'rgb(0, 82, 204)',
-              '@font-size-base': '14px'
-            }
+export const style = {
+  postcss: {
+    plugins: [require('tailwindcss'), require('autoprefixer')]
+  }
+};
+export const plugins = [
+  {
+    plugin: CracoLessPlugin,
+    options: {
+      lessLoaderOptions: {
+        lessOptions: {
+          javascriptEnabled: true,
+          modifyVars: {
+            '@primary-color': 'rgb(0, 82, 204)',
+            '@font-size-base': '14px'
           }
         }
       }
     }
-  ]
-};
+  }
+];
